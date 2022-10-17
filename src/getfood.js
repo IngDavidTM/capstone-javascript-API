a307d96f6ed899a6dca28e1f684d7d25fc07dd5e
Shedrack Sunday <Hannania002@gmail.com>
2022-10-17 21:33:25 +0100

    Add header changes

---
+++ src/getfood.js
@@ -0,0 +1,12 @@
+const displayData = (arr) => {
+   const board = document.getElementById('food');
+   board.innerHTML = '';
+
+   arr.forEach((food) => {
+    const item = document.createElement('Div');
+    item.innerHTML = `img src=&{food.strMealT}`
+    
+   });
+
+
+}
