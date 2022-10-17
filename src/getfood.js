5f6180bc3f9f294c36578a5e50d609e061d0c6fc
Shedrack Sunday <Hannania002@gmail.com>
2022-10-17 22:19:29 +0100

    Add file changes

--- src/getfood.js
+++ src/getfood.js
@@ -1,9 +1,11 @@
 const displayData = (arr) => {
-  const board = document.getElementById("food");
-  board.innerHTML = "";
+  const board = document.getElementById('food');
+  board.innerHTML = '';
 
   arr.forEach((food) => {
-    const item = document.createElement("Div");
+    const item = document.createElement('Div');
     item.innerHTML = `img src=&{food.strMealT}`;
+    item.id = food.idMeal;
+    board.appendChild(item);
   });
 };
