06c49ee659317906a1790dfaabdc4dd387b9a2dd
Shedrack Sunday <Hannania002@gmail.com>
2022-10-18 20:42:40 +0100

    Add getfoods file

---
+++ src/getfoods.js
@@ -0,0 +1,36 @@
+const displayData = (arr) => {
+    const board = document.querySelector('.container-food-cards');
+    board.innerHTML = '';
+    arr.forEach((food) => {
+      const item = document.createElement('article');
+      item.classList.add('card-food');
+      item.innerHTML = `
+          <div class="card-title">
+            <h5>${food.strMeal}</h5>
+          </div>
+          <div class="btn-recipe">
+            <button type="button">Recipe</button>
+          </div>
+          <div class="btn-liked">
+            <button type="button">Like</button>
+          </div>
+          <div class="card-img">
+           <img src="${food.strMealThumb}" class="img-food">        
+          </div>     
+      `;
+      item.id = food.idMeal;
+      // console.log(food);
+      board.appendChild(item);
+    });
+  };
+  
+  const getAllData = async (url) => {
+    const request = new Request(url);
+    const response = await fetch(request);
+    const responseJson = await response.json();
+    const responsInfo = responseJson.meals;
+  
+    displayData(responsInfo);
+  };
+  
+  export { getAllData as default };
