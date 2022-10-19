fecd71b8cf79e7e13455350a1fbe3314edc2ffaa
Shedrack Sunday <Hannania002@gmail.com>
2022-10-19 08:10:39 +0100

    Add event listner to index.js

--- src/index.js
+++ src/index.js
@@ -3,4 +3,14 @@ import getAllData from './modules/getfoods.js';
 
 const allBeefURL = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Beef';
 
-getAllData(allBeefURL);
+const [liBeefElement] = document.querySelectorAll('li');
+let foodChosen = liBeefElement;
+
+liBeefElement.addEventListener('click', () => {
+    foodChosen = liBeefElement;
+    liBeefElement.classList.add('selected');
+    foodChosen.textContent = 'Beef';
+    getAllData(allBeefURL, foodChosen);
+  });
+
+getAllData(allBeefURL, foodChosen);
