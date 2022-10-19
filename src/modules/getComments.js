f140c51f440f9350cd4057d55198b6112a311104
IngDavidTM <david-1011@hotmail.com>
2022-10-18 22:42:05 -0500

    add getComments function

---
+++ src/modules/getComments.js
@@ -0,0 +1,13 @@
+const getComments = async (index) => {
+  const get = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/JGerHk43c1Y5J5m1thia/comments?item_id=item${index}`);
+  const arr = await get.json();
+  const div = document.getElementById('divComments');
+  document.getElementById('divComments').innerHTML = '';
+  arr.forEach((element) => {
+    const p = document.createElement('p');
+    p.innerHTML = `${element.creation_date}  ${element.username}: ${element.comment}`;
+    div.appendChild(p);
+  });
+};
+
+export default getComments;
