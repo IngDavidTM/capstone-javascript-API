62253ed3b25789356a6aaa3938bef08cdfcac40e
Shedrack Sunday <Hannania002@gmail.com>
2022-10-20 00:43:10 +0100

    ADD LINTER ERRORS ON QUOTES

--- src/modules/getfoods.js
+++ src/modules/getfoods.js
@@ -1,14 +1,13 @@
 import popUp from './popUp.js';
 import displayCounter from './counterFile.js';
-import { addLikeToItem, getLikesToItems} from './like.js';
-
+import { addLikeToItem, getLikesToItems } from './like.js';
 
 const displayData = async (arr) => {
   const board = document.querySelector('.container-food-cards');
   board.innerHTML = '';
   arr.forEach((food) => {
     const item = document.createElement('article');
-    item.id = food.idMeal
+    item.id = food.idMeal;
     item.classList.add('card-food');
     item.innerHTML = `
           <div class='card-title'>
@@ -25,26 +24,26 @@ const displayData = async (arr) => {
           </div>     
       `;
 
-      const btnLikedElement = item.querySelector('.btn-liked');
-      const printLike = (data) => {
-        const likesReturned = data.find(
-          (element) => element.item_id === food.idMeal
-        );
-        btnLikedElement.innerHTML =
-          likesReturned !== undefined
-            ? `<i class="fas fa-heart"></i> (${likesReturned.likes})`
-            : '<i class="far fa-heart"></i> (0)';
-      };
+    const btnLikedElement = item.querySelector('.btn-liked');
+    const printLike = (data) => {
+      const likesReturned = data.find(
+        (element) => element.item_id === food.idMeal
+      );
+      btnLikedElement.innerHTML =
+        likesReturned !== undefined
+          ? `<i class="fas fa-heart"></i> (${likesReturned.likes})`
+          : '<i class="far fa-heart"></i> (0)';
+    };
+    getLikesToItems()
+      .then(printLike)
+      .catch((e) => console.log(e));
+
+    btnLikedElement.addEventListener('click', () => {
+      addLikeToItem(food.idMeal);
       getLikesToItems()
         .then(printLike)
         .catch((e) => console.log(e));
-  
-      btnLikedElement.addEventListener('click', () => {
-        addLikeToItem(food.idMeal);
-        getLikesToItems()
-          .then(printLike)
-          .catch((e) => console.log(e));
-      }); 
+    });
     board.appendChild(item);
   });
   const buttonComments = document.querySelectorAll('.btn-recipe');
